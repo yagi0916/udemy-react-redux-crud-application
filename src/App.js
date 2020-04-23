6e70b9000c0ab1340cb9958509077bfc7b04c014
yagi5369 <yagi.dk.0916@gmail.com>
2020-04-23 17:03:11 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <div><h1>Hello-world</h1></div>;
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I am clocked.")}} />
+    </React.Fragment>
+  )
 }
 export default App;
